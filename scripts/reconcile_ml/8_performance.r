830e6f3cfbab8bf94ba6e2bafeb2890fd405048d
Alberson Miranda <albersonmiranda@hotmail.com>
2023-10-04 08:54:53 -0300

    performance script

---
+++ scripts/reconcile_ml/8_performance.r
@@ -0,0 +1,193 @@
+### PERFORMANCE
+
+
+# pacotes
+library(fabletools)
+
+# juntando predições em um único dataframe
+preds = lapply(c("xgb", "ranger", "glmnet", "lasso", "ridge"), function(learner) {
+  preds = readRDS(paste0("data/preds_", learner, ".RDS"))
+  preds = lapply(preds[[1]], function(df) data.table::as.data.table(df) |> subset(select = response))
+  preds = do.call(cbind, preds)
+  # adicionando coluna ref
+  preds$ref = tsibble::yearmonth(seq(as.Date("2022-01-01"), as.Date("2022-12-01"), by = "month"))
+  return(preds)
+})
+
+# nomeando a lista
+names(preds) = c("xgb", "ranger", "glmnet", "lasso", "ridge")
+
+# remover primeiro caractere do nome das colunas
+preds = lapply(preds, function(df) {
+  names(df) = gsub("^.", "", names(df))
+  return(df)
+})
+
+# renomeando coluna ef para ref
+preds = lapply(preds, function(df) {
+  names(df)[names(df) == "ef"] = "ref"
+  return(df)
+})
+
+# substituir o texto "baixo_guandu" para "BAIXO_GUANDU" no nome das colunas
+preds = lapply(preds, function(df) {
+  names(df) = gsub("baixo_guandu", "BAIXO_GUANDU", names(df))
+  return(df)
+})
+
+# alongando dataframe preds
+preds = lapply(preds, function(df) {
+  df = df |>
+    tidyr::pivot_longer(
+      cols = -ref,
+      names_to = c("cnpj_agencia", "nome", "nome_microrregiao", "nome_mesorregiao", "verbete"),
+      values_to = "prediction",
+      names_sep = "x"
+    )
+  return(df)
+})
+
+# revertendo substituição de texto
+preds = lapply(preds, function(df) {
+  df$nome = gsub("BAIXO_GUANDU", "baixo_guandu", df$nome)
+  return(df)
+})
+
+# substituindo _ por espaço em todo o dataframe
+preds = lapply(preds, function(df) {
+  data = within(df, {
+    nome = gsub("_", " ", nome)
+    nome_microrregiao = gsub("_", " ", nome_microrregiao)
+    nome_mesorregiao = gsub("_", " ", nome_mesorregiao)
+    verbete = gsub("_", " ", verbete)
+  })
+
+  preds = data$prediction
+
+  # remover espaços no início e fim das strings
+  data = lapply(data, function(col) {
+    if (!is.numeric(col)) {
+      col = trimws(col)
+      return(col)
+    }
+  })
+
+  # adicionando novamente coluna prediction
+  data$prediction = preds
+
+  return(data.frame(data))
+})
+
+# remover sufixo " true.response" da coluna verbete
+preds = lapply(preds, function(df) {
+  df$verbete = gsub(" true.response", "", df$verbete)
+  return(df)
+})
+
+# adicionar coluna modelo
+preds = lapply(names(preds), function(learner) {
+  preds[[learner]]$modelo = learner
+  return(preds[[learner]])
+})
+
+# juntando todos os dataframes em um único
+preds = do.call(rbind, preds)
+
+# estban dataset
+estban = readRDS("data/estban.rds") |>
+  tsibble::filter_index("2022 jan" ~ "2022 dec")
+
+# remover agregados
+estban = estban |>
+  subset(
+    !is_aggregated(nome_mesorregiao)
+    & !is_aggregated(nome_microrregiao)
+    & !is_aggregated(nome)
+    & !is_aggregated(verbete)
+    & !is_aggregated(cnpj_agencia)
+  ) |>
+  as.data.frame()
+
+# convertendo coluna ref para yearmonth
+preds$ref = as.character(preds$ref)
+estban$ref = as.character(estban$ref)
+
+# convertendo colunas chr* do dataframe estban para character
+estban = within(estban, {
+  nome_mesorregiao = as.character(nome_mesorregiao)
+  nome_microrregiao = as.character(nome_microrregiao)
+  nome = as.character(nome)
+  verbete = as.character(verbete)
+  cnpj_agencia = as.character(cnpj_agencia)
+})
+
+# limpando dataframe estban
+estban = within(estban, {
+  nome_mesorregiao = iconv(tolower(gsub("-", " ", nome_mesorregiao)), "LATIN1", "ASCII//TRANSLIT")
+  verbete = iconv(tolower(verbete), "LATIN1", "ASCII//TRANSLIT")
+  nome_microrregiao = iconv(tolower(nome_microrregiao), "LATIN1", "ASCII//TRANSLIT")
+  nome = iconv(tolower(nome), "LATIN1", "ASCII//TRANSLIT")
+})
+
+# merge entre estban e preds
+data = merge(estban, preds)
+
+# teste se merge foi completo
+nrow(data) == 5 * nrow(estban)
+
+# agregando
+data = data |>
+  transform(ref = tsibble::yearmonth(as.Date(paste0(ref, "-01"), format = "%Y %b-%d"))) |>
+  tsibble::as_tsibble(
+    key = c(
+      "nome_mesorregiao",
+      "nome_microrregiao",
+      "nome",
+      "cnpj_agencia",
+      "verbete",
+      "modelo"
+    ),
+    index = ref
+  ) |>
+  fabletools::aggregate_key(
+    (nome_mesorregiao / nome_microrregiao / nome / cnpj_agencia) * verbete * modelo,
+    saldo = sum(saldo),
+    prediction = sum(prediction)
+  )
+
+# adicionando coluna erro
+data$erro = data$prediction - data$saldo
+
+# combinações para acurácia
+combinacoes = list(
+  agregado = "is_aggregated(verbete) & is_aggregated(cnpj_agencia) & is_aggregated(nome) & is_aggregated(nome_microrregiao) & is_aggregated(nome_mesorregiao) & !is_aggregated(modelo)", # nolint
+  mesorregiao = "is_aggregated(verbete) & is_aggregated(cnpj_agencia) & is_aggregated(nome) & is_aggregated(nome_microrregiao) & !is_aggregated(nome_mesorregiao) & !is_aggregated(modelo)", # nolint
+  microrregiao = "is_aggregated(verbete) & is_aggregated(cnpj_agencia) & is_aggregated(nome) & !is_aggregated(nome_microrregiao) & !is_aggregated(nome_mesorregiao) & !is_aggregated(modelo)", # nolint
+  municipio = "is_aggregated(verbete) & is_aggregated(cnpj_agencia) & !is_aggregated(nome) & !is_aggregated(nome_microrregiao) & !is_aggregated(nome_mesorregiao) & !is_aggregated(modelo)", # nolint
+  agencia = "is_aggregated(verbete) & !is_aggregated(cnpj_agencia) & !is_aggregated(nome) & !is_aggregated(nome_microrregiao) & !is_aggregated(nome_mesorregiao) & !is_aggregated(modelo)", # nolint
+  verbete = "!is_aggregated(verbete) & !is_aggregated(cnpj_agencia) & !is_aggregated(nome) & !is_aggregated(nome_microrregiao) & !is_aggregated(nome_mesorregiao) & !is_aggregated(modelo)", # nolint
+  hierarquia = "!is_aggregated(modelo)" # nolint
+)
+
+# acurácia
+data_acc = lapply(names(combinacoes), function(nivel) {
+  data = tsibble::as_tibble(data) |>
+    dplyr::filter(!!rlang::parse_expr(combinacoes[[nivel]])) |>
+    dplyr::mutate(se = erro ^ 2, modelo = as.character(modelo)) |>
+    dplyr::group_by(modelo) |>
+    dplyr::summarise(sse = sum(se), n = dplyr::n()) |>
+    dplyr::mutate(rmse = sqrt(sse / n))
+  data$serie = nivel
+
+  return(data)
+})
+
+# agregando dataframes
+acuracia_ml = do.call("rbind", data_acc) |>
+  subset(select = -c(n, sse)) |>
+  tidyr::pivot_wider(
+    names_from = serie,
+    values_from = c(rmse)
+  ) |>
+  as.data.frame(t()) |>
+  tibble::as_tibble()
