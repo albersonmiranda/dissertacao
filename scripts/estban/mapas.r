f6495b508dede1fb34f7ac5b5512cd3a5f3fee7a
Alberson Miranda <albersonmiranda@hotmail.com>
2023-11-23 11:20:02 -0300

    draft

--- scripts/estban/mapas.r
+++ scripts/estban/mapas.r
@@ -15,6 +15,12 @@ mesorregioes = geobr::read_meso_region(
   year = 2020
 )
 
+# microrregiões
+microrregioes = geobr::read_micro_region(
+  code_micro = "ES",
+  year = 2020
+)
+
 # plots
 plot_br = brasil |>
   ggplot(aes(fill = abbrev_state)) +
@@ -23,8 +29,8 @@ plot_br = brasil |>
     values = c(ES = "#56AF31"),
     na.value = "#003468"
   ) +
-  theme_void() +
-  theme(legend.position = "none")
+  labs(fill = "Estado") +
+  theme_void()
 
 plot_meso = mesorregioes |>
   ggplot(aes(fill = name_meso)) +
@@ -33,10 +39,27 @@ plot_meso = mesorregioes |>
   labs(fill = "mesorregiões") +
   theme_void()
 
+plot_micro = mesorregioes |>
+  ggplot(aes(fill = name_meso)) +
+  geom_sf() +
+  geom_sf(data = microrregioes, fill = NA) +
+  geom_sf_text(data = microrregioes, aes(label = name_micro, fill = NA, color = "microrregiões")) +
+  scale_color_manual(name = NULL, values = c("microrregiões" = "#000000")) +
+  coord_sf(xlim = c(-42, -39)) +
+  labs(fill = "mesorregiões") +
+  theme_void()
+
 # exportar
 ggsave(
   filename = "img/mapa-brasil.png",
   plot = plot_br,
+  width = 1200,
+  units = "px"
+)
+
+ggsave(
+  filename = "img/mapa-microrregioes.png",
+  plot = plot_micro,
   width = 1200,
   units = "px"
 )
